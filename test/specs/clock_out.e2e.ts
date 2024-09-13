6f962d4058dbde0379b65295a3e4d5896f925cbb
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2024-09-14 06:46:56 +0900

    有休チェック

--- test/specs/clock_out.e2e.ts
+++ test/specs/clock_out.e2e.ts
@@ -1,15 +1,22 @@
 import { browser } from '@wdio/globals';
 import LoginPage from '../pageobjects/login.page.js';
 import constants from './constants.js';
+import { checkIsMyHoliday } from './utils';
 
 describe('KintaiPlus', () => {
     it('clock_out', async () => {
-        await LoginPage.open()
-        await LoginPage.login(constants.KTP_ID, constants.KTP_PASSWORD);
-        await browser.pause(5000);
-        console.log("start clock out");
-        await LoginPage.clockOut();
-        await browser.pause(5000);
+        var holiday = await checkIsMyHoliday();
+        if (!holiday) {
+            console.log("今日は祝日ではありません");
+            await LoginPage.open()
+            await LoginPage.login(constants.KTP_ID, constants.KTP_PASSWORD);
+            await browser.pause(5000);
+            console.log("start clock out");
+            await LoginPage.clockOut();
+            await browser.pause(5000);
+        } else {
+            console.log("今日休み");
+        }
     })
 })
 
--- test/specs/constants.ts
+++ test/specs/constants.ts
@@ -1,4 +1,5 @@
 export default {
     KTP_ID: process.env.KTP_ID || '',
     KTP_PASSWORD: process.env.KTP_PASSWORD || '',
+    KTP_MY_HOLIDAYS_URL: "https://finbertmds.github.io/holidays.html",
 }
