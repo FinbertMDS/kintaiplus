8bd2227fcad319ff4ffe4ccbb7eb4f515f2e1a1e
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2024-09-18 08:26:30 +0900

    fix test

--- test/specs/clock_out.e2e.ts
+++ test/specs/clock_out.e2e.ts
@@ -1,11 +1,11 @@
 import { browser } from '@wdio/globals';
 import LoginPage from '../pageobjects/login.page.js';
+import Utils from '../pageobjects/utils.js';
 import constants from './constants.js';
-import { checkIsMyHoliday } from './utils';
 
 describe('KintaiPlus', () => {
     it('clock_out', async () => {
-        var holiday = await checkIsMyHoliday();
+        var holiday = await Utils.checkIsMyHoliday();
         if (!holiday) {
             console.log("今日は祝日ではありません");
             await LoginPage.open()
